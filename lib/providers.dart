ae54325216fc04e6a7508514847f95df404d9876
arasan01 <sinzan062871+git@gmail.com>
2022-02-07 05:30:44 +0900

    add providerの追加

--- lib/providers.dart
+++ lib/providers.dart
@@ -1,7 +1,9 @@
 import 'package:apack/entity/compression_image_options.dart';
+import 'package:apack/entity/process_image.dart';
 import 'package:apack/entity/theme.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:image_compression_flutter/image_compression_flutter.dart';
 
 // NavigationPane selected index
 final paneIndexProvider = StateProvider<int>((ref) => 0);
@@ -16,6 +18,14 @@ final remainInfoBadgeProvider = Provider<InfoBadge?>((ref) {
         )
       : null;
 });
+
+// Format conversion select file path
+final formatConversionFilePathProvider = StateProvider<XFile?>((_) => null);
+final formatConversionOutputTypeProvider =
+    StateProvider<ImageOutputType>((_) => ImageOutputType.jpg);
+final formatConversionProcessImageProvider =
+    StateNotifierProvider.autoDispose<ProcessImageNotifier, ProcessImage?>(
+        (_) => ProcessImageNotifier());
 
 // Theme provider
 final appThemeProvider = StateNotifierProvider<AppThemeNotifier, AppTheme>(
